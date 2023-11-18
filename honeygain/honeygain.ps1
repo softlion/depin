0eeb69c86eade1cea33893507f02a37867b63619
Benjamin Mayrargue <benjamin@vapolia.fr>
2023-11-18 13:44:54 +0100

    correct honeygain

---
+++ honeygain/honeygain.ps1
@@ -0,0 +1,100 @@
+#github.com/softlion/depin
+
+param (
+  [string]$IP = $null,
+  [string]$Email = $null,
+  [string]$Password = $null,
+  [string]$DeviceName = $null,
+  [switch]$Debug
+  )
+
+function Main() {
+  $ok = CheckRequirements
+  if(-not $ok) {
+      return;
+  }
+
+    Write-Host @"
+--------------------------------------
+Honeygain install
+For any firmware with Docker (Pisces) or Balena (Sencap, Nebra)
+https://github.com/softlion/depin
+--------------------------------------
+SSH port defaults to 22
+SSH user defaults to root
+"@
+
+  if ([string]::IsNullOrEmpty($IP)) {
+    Write-Host "Enter ssh IP of target device (ex: admin@192.168.10.22 for pisces, 192.168.10.22:22222 for nebra/sensecap)"
+    $IP = Read-Host "IP:"
+  }
+
+
+  $User, $IP, $Port = Get-IPAndPortAndUser $IP  "root" 22
+  Write-Host "Connecting to $($User)@$($IP):$($Port)"
+
+  if(!$Debug) {
+    $all = iwr 'https://raw.githubusercontent.com/softlion/depin/main/honeygain/honeygain.sh' -UseBasicParsing | % Content
+  } else {
+    $all = Get-Content -Raw -Path (Join-Path $PSScriptRoot "honeygain.sh")
+  }
+
+  if (![string]::IsNullOrEmpty($Email)) {
+    $all = "ACCOUNT_EMAIL=$Email; `n" + $all
+  }
+  if (![string]::IsNullOrEmpty($Password)) {
+    $all = "ACCOUNT_PASSWORD=$Password; `n" + $all
+  }
+  if (![string]::IsNullOrEmpty($DeviceName)) {
+    $all = "DEVICE_NAME=$DeviceName; `n" + $all
+  }
+
+  $all = $all -replace "`r"
+  ssh -t -p $Port $User@$IP $all
+
+  Write-Host "Finished"
+}
+
+function Get-IPAndPortAndUser() {
+  param (
+    [string]$Value,
+    [string]$DefaultUser,
+    [int]$DefaultPort
+  )
+
+  $pattern = "^((?<user>[^@]+)@)?(?<ip>[^:]+)(:(?<port>\d+))?$"
+  $match = $Value -match $pattern
+  
+  if ($match) {
+      $extractedUser = if ($matches['user']) { $matches['user'] } else { $DefaultUser }
+      $extractedIP = $matches['ip']
+      $extractedPort = [int]($matches['port'] ? $matches['port'] : $DefaultPort)
+      return $extractedUser, $extractedIP, $extractedPort
+  } else {
+      throw "Invalid IP format"
+  }
+}
+
+function CheckRequirements() {
+  $requiredPSVersion = [version]"7.3.4"
+  $currentPSVersion = $PSVersionTable.PSVersion
+
+  if ($currentPSVersion -lt $requiredPSVersion) {
+      Write-Host "ERROR: powershell version is too old"
+      Write-Host "Version 7.3.4 or greater is required."
+      Write-Host "Current PowerShell version is $currentPSVersion"
+      Write-Host ""
+      Write-Host "https://apps.microsoft.com/store/detail/powershell/9MZ1SNWT0N5D"
+      Write-Host "or"
+      Write-Host "winget install Microsoft.Powershell"
+      Write-Host ""
+      Write-Host "Once installed, to open powershell, enter 'pwsh' in the windows start menu"
+      return $false;
+  }
+
+  return $true
+}
+
+
+# start
+Main
---
+++ honeygain/honeygain.sh
@@ -0,0 +1,148 @@
+#create folders
+depinFolder=/mnt/data/depin;
+honeygainFolder=$depinFolder/honeygain;
+folder=$honeygainFolder
+if [ ! -d $folder ]; then mkdir -p $folder; fi;
+
+
+function onBoardHoneygain(){
+    if [ -z "$ACCOUNT_EMAIL" ]; then
+        echo "Enter your honeygain email"
+        ACCOUNT_EMAIL=$(prompt_with_default "email" "")
+        export ACCOUNT_EMAIL
+    fi
+    if [ -z "$ACCOUNT_PASSWORD" ]; then
+        echo "Enter your honeygain password"
+        ACCOUNT_PASSWORD=$(prompt_with_default "password" "")
+        export ACCOUNT_PASSWORD
+    fi
+    if [ -z "$DEVICE_NAME" ]; then
+        echo "Enter a name for this device"
+        DEVICE_NAME=$(prompt_with_default "device name" "")
+        export DEVICE_NAME
+    fi
+}
+
+
+function prompt_with_default() {
+  local prompt="$1"
+  local default_value="$2"
+  local user_input
+
+  while true; do
+    read -p "$prompt [$default_value]: " user_input
+
+    if [ -z "$user_input" ]; then
+      user_input="$default_value"
+    fi
+
+    if [ -n "$user_input" ]; then
+      break
+    else
+      echo "Value cannot be empty. Please enter a value."
+    fi
+  done
+
+  echo "$user_input"
+}
+
+
+
+function createSupervisorScript() {
+    installScript='
+
+startHoneygain() {
+    container_name=honeygain
+    if docker container inspect "$container_name" >/dev/null 2>&1; then docker rm -f $container_name; fi;
+
+    docker run --privileged --rm tonistiigi/binfmt --install amd64
+
+    docker run -d \
+    --name honeygain \
+    --restart unless-stopped \
+    --label=com.centurylinklabs.watchtower.enable=true \
+    --platform=linux/amd64 \
+    honeygain/honeygain \
+    -tou-accept \
+    -email '"$ACCOUNT_EMAIL"' \
+    -pass '"$ACCOUNT_PASSWORD"' \
+    -device '"$DEVICE_NAME"'
+}
+
+run() {
+    echo "starting honeygain"
+    startHoneygain
+
+    while true; do
+        sleep 3600
+    done
+}
+
+run
+'
+
+ echo "$installScript" > $honeygainFolder/honeygain-supervisor.sh
+}
+
+function startSupervisor(){
+    container_name=honeygain-supervisor
+    if $hypervisor container inspect "$container_name" >/dev/null 2>&1; then $hypervisor rm -f $container_name; fi;
+
+    $hypervisor run -d --restart unless-stopped --name $container_name \
+        -v /var/run/$hypervisor.sock:/var/run/docker.sock \
+        -v $honeygainFolder/honeygain-supervisor.sh:/run.sh \
+        --label=com.centurylinklabs.watchtower.enable=true \
+        docker sh -c "sh /run.sh"
+}
+
+
+function checkBalenaDocker() {
+
+  if command -v balena &>/dev/null; then   balena_installed=1; else   balena_installed=0; fi
+  if command -v docker &>/dev/null; then   docker_installed=1; else   docker_installed=0; fi
+
+  if [[ $docker_installed -eq 0 && $balena_installed -eq 0 ]]; then
+      echo "Neither Docker nor Balena are installed. Exiting"
+      exit 100;
+  elif [[ $balena_installed -eq 1 ]]; then
+      echo "balena"
+  else
+      echo "docker"
+  fi
+}
+
+function displayQr() {
+echo "--------------------------------------"
+echo "You liked this script ?"
+echo "Send me some crypto tokens :)"
+echo ""
+ echo "█▀▀▀▀▀█ ▀▀▀▀▄█▄▀  ▄▄▀▄▀▀█ █▀▀▀▀▀█  "
+ echo "█ ███ █ ▄▄▄▀▀ ▀█▄█▀ ▀▀ █  █ ███ █  "
+ echo "█ ▀▀▀ █ ███  █▄▄▀▄ ▀▄  ▀▄ █ ▀▀▀ █  "
+ echo "▀▀▀▀▀▀▀ █▄▀ █ █ ▀▄▀▄█▄█▄▀ ▀▀▀▀▀▀▀  "
+ echo "▀▄ ▄█▄▀▀▀█▄▀  ▀█▄█▄ ▄▀▄▄▄█▀█▀█▄▄▀  "
+ echo "█ ▄ █ ▀▄ █▀▄██▀▀█ ▀  █▀██ ▄ █▀ █   "
+ echo " ▄██▄█▀ █▄▀▀▀▀▄ ▀ ▄▄█▀▄▄▄▀▀▄▀ ▄▀▀  "
+ echo "████▄█▀▀█▄ █▄▀██▄██ ▀█▀▀ █▀ ▄█▀█   "
+ echo "▄ ▀▀  ▀▀ ▄█▄▀█▄▄▀▀▀▀▄▀▄▄▀▀▀█▀▀ ▄█  "
+ echo "▀█  ▀▀▀██▄████▄▀▀█▄ ▄▄▀█    █▀ ▀▄  "
+ echo "██▄▀▄▄▀▄█▄ █ ▀▄ ▄ ▀▀▀█▀▀▀▄█▀▀ ▄█▀  "
+ echo "  ██▀ ▀▄▄ ▄  ▄▀ ▀▄▀ ███▀█ █ ▄  █▄  "
+ echo "▀▀  ▀ ▀ ▄▄█  ▀▀█▀█▀▀▄▀▀▄█▀▀▀█▄█▀▀  "
+ echo "█▀▀▀▀▀█ ▀ ▄▄█▀██▄ ▀▄██▀ █ ▀ █      "
+ echo "█ ███ █ ▀████▀▀ ▀▄▄█▀▀▄ ███▀█▀▄ ▀  "
+ echo "█ ▀▀▀ █  ▀   ▀ █▄█ ▄ █ ▀▄█▀▄▀▄▀    "
+ echo "▀▀▀▀▀▀▀ ▀▀  ▀      ▀ ▀  ▀▀   ▀  ▀  "
+echo "--------------------------------------"
+}
+
+echo "Starting honeygain installation"
+echo "(you can run this script multiple times without any issue)"
+
+hypervisor=$(checkBalenaDocker)
+
+onBoardHoneygain
+createSupervisorScript
+startSupervisor
+displayQr
+echo "finished"
