007075a4a639b8302f4714fefde8b48b72d31e17
ZeBen <benjamin@vapolia.fr>
2023-12-21 23:04:18 +0100

    Update streamr.sh

--- streamr/streamr.sh
+++ streamr/streamr.sh
@@ -36,7 +36,7 @@ function installStreamr() {
   $runHypervisor run -d --name "$container" \
     --restart unless-stopped \
     -p 32200:32200 \
-    -e LOG_LEVEL=trace
+    -e LOG_LEVEL=trace \
     -v "$projectFolder":/home/streamr/.streamr \
     -v "$projectFolder":/root/.streamr \
     --label=com.centurylinklabs.watchtower.enable=true \
